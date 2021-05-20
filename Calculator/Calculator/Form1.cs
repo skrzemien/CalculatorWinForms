439dc1da58850a20dd3caea95fadcddbf6dbaa84
Sebastian Krzemień <59866137+skrzemien@users.noreply.github.com>
2021-05-20 16:57:08 +0200

    Done

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PerformCalculation(Operation previousOperation)
+        {
+            List<double> lstNums = new List<double>();
+            switch (previousOperation)
+            {
+                case Operation.Add:
+                    lstNums = txtDisplay.Text.Split('+').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] + lstNums[1]).ToString();
+                    break;
+                case Operation.Sub:
+                    lstNums = txtDisplay.Text.Split('-').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] - lstNums[1]).ToString();
+                    break;
+                case Operation.Mul:
+                    lstNums = txtDisplay.Text.Split('*').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] + lstNums[1]).ToString();
+                    break;
+                case Operation.Div:
+                    try
+                    {
+                        lstNums = txtDisplay.Text.Split('/').Select(double.Parse).ToList();
+                        txtDisplay.Text = (lstNums[0] + lstNums[1]).ToString();
+                    }
+                    catch (DivideByZeroException)
+                    {
+
+                        txtDisplay.Text = "EEEE";
+                    }
+                    
+                    break;
+                case Operation.None:
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtDisplay.Text);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            previousOperation = Operation.None;
+            txtDisplay.Clear();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if(txtDisplay.Text.Length > 0)
+            {
+                double d;
+                if(!double.TryParse(txtDisplay.Text[txtDisplay.Text.Length-1].ToString(), out d))
+                {
+                    previousOperation = Operation.None;
+                }
+                txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
+            }
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            if (previousOperation != Operation.None)
+                PerformCalculation(previousOperation);
+
+            previousOperation = Operation.Div;
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            if (previousOperation != Operation.None)
+                PerformCalculation(previousOperation);
+
+            previousOperation = Operation.Mul;
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            if (previousOperation != Operation.None)
+                PerformCalculation(previousOperation);
+
+            previousOperation = Operation.Sub;
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (previousOperation != Operation.None)
+                PerformCalculation(previousOperation);
+
+            previousOperation = Operation.Add;
+            txtDisplay.Text += (sender as Button).Text;
+        }
+
+
+        private void btnNum_Click(object btn, EventArgs e)
+        {
+            txtDisplay.Text += (btn as Button).Text;
+        }
+
+        enum Operation
+        {
+            Add,
+            Sub,
+            Mul,
+            Div,
+            None
+        }
+        static Operation previousOperation = Operation.None;
+
+        private void btnRes_Click(object sender, EventArgs e)
+        {
+            if (previousOperation == Operation.None)
+                return;
+            else
+                PerformCalculation(previousOperation);
+        }
+    }
+}
